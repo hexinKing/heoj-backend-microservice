203924a05ff409efbefe817ba63623e240df7d40
hexinKing <m15819244076@163.com>
2024-08-19 22:33:14 +0800

    微服务划分及功能介绍

---
+++ src/main/java/com/heoj/heojbackendmicroservice/HeojBackendMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.heoj.heojbackendmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeojBackendMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeojBackendMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/heoj/heojbackendmicroservice/HeojBackendMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heoj.heojbackendmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeojBackendMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
