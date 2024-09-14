15e53418f908c14d151a990d94377c285f52b590
hexinKing <m15819244076@163.com>
2024-09-14 09:51:55 +0800

    新增了heoj-backend-file-service：系统文件模块，比如用户头像上传等

---
+++ hxoj-backend-file-service/src/main/java/com/hexin/hxojbackendgateway/service/FileService.java
@@ -0,0 +1,14 @@
+package com.hexin.hxojbackendgateway.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+
+public interface FileService {
+    /**
+     * 上传头像到OSS
+     *
+     * @param file
+     * @return
+     */
+    String uploadFileAvatar(MultipartFile file);
+}
